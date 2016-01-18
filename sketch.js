b53ed53f6d1fc8a3b8c89cfeb016ebb94b8d119e
Will Field <willjfield@gmail.com>
2016-01-18 12:59:50 -0500

    save position of keys in time

--- sketch.js
+++ sketch.js
@@ -7,21 +7,21 @@ function setup(){
 	createCanvas(windowWidth,windowHeight)
 	radius = 100
 	animation = new Timeline('radius')
-	animation.playSpeed = 2
+	animation.playSpeed = 1
 
-	animation.addKey(200, 2,'ease')
+	animation.addKey(200,2,'ease')
 	animation.addKey(100,2,'ease')
-	animation.addKey(200,2,'linear')
-	animation.addKey(100,2,'linear')
+	animation.addKey(200,3,'linear')
+	animation.addKey(100,3,'linear')
 	//animation.stretchKeys(5)
-	animation.startKey(5)
-	for(var k in animation.keys){
+	//animation.startKey(5)
+	/*for(var k in animation.keys){
 		console.log(animation.keys[k].time)
-	}
+	}*/
 	fill(255)
 	textAlign(CENTER)
 	textSize(32)
-
+	console.log(animation.keys)
 }
 function draw(){
 	background(0)
@@ -39,11 +39,13 @@ function draw(){
 	rect(0,0,radius,radius*2);
 	pop();		
 		
-		
+	
 	text("Click once to play the animation",windowWidth/2,windowHeight-100)
 }
 
 function mousePressed(){
+	//animation.shiftKeyPositions(3)
+	//console.log(animation.keys)
 	animation.play()
 }
 
@@ -57,11 +59,12 @@ var Timeline = function(_variable){
 	this.variable = {
 		name: _variable,
 		value: window[_variable]
-	}
+	}	
 	this.keys = []
 	this.isPlaying = false
 	this.startTime = 0
 	this.playSpeed = 1
+	this.addKey(this.variable.value,0,'linear')
 }
 
 Timeline.prototype.stretchKeys = function(amount){
@@ -69,16 +72,20 @@ Timeline.prototype.stretchKeys = function(amount){
 		entry.time*=amount
 	});
 }
-
-Timeline.prototype.startKey = function(amount){
-	var key = {
-		targetValue: this.variable.value,
-		time: amount,
-		interpolation: 'linear'
-	}
-	this.keys.unshift(key)
+Timeline.prototype.setKeyPositions = function(){
+	var curPos = 0
+	this.keys.forEach(function(k){
+		curPos+=k.time
+		k.keyPosition = curPos
+	})
 }
-
+Timeline.prototype.shiftKeyPositions = function(amount){
+	var curPos = amount
+	this.keys.forEach(function(k){
+		curPos+=k.time
+		k.keyPosition = curPos
+	})
+}
 Timeline.prototype.addKey = function(targetValue,time,interpolation){
 	var key = {
 		targetValue: targetValue,
@@ -86,6 +93,7 @@ Timeline.prototype.addKey = function(targetValue,time,interpolation){
 		interpolation: interpolation
 	}	
 	this.keys.push(key)
+	this.setKeyPositions();
 }
 
 Timeline.set = function (_variable, value){
@@ -100,7 +108,7 @@ Timeline.prototype.play = function(){
 	var variable = this.variable
 	var playSpeed = this.playSpeed
 	var startTime = this.startTime
-	var keyNum = 0
+	var keyNum = 1
 
 	if(!isPlaying){
 		startTime = millis()
@@ -110,23 +118,18 @@ Timeline.prototype.play = function(){
 
 	var animate = function(){
 		var elapsedTime = millis()-startTime;
-		console.log('start value:' + startValue)
-		console.log('target value:' + keys[keyNum].targetValue)
-		if(elapsedTime<keys[keyNum].time*1000/playSpeed){
+		//console.log('start value:' + startValue)
+		//console.log('target value:' + keys[keyNum].targetValue)
+		if(elapsedTime<(keys[keyNum].time*1000/playSpeed)){
 			setTimeout(animate,1000/getFrameRate())	
 			var curTime=(elapsedTime/(keys[keyNum].time*1000))*playSpeed			
 			if(keys[keyNum].interpolation=='linear'){	
 				curValue=startValue+(keys[keyNum].targetValue-startValue)*curTime
-				//lerp actually works too with the below expression:
-				//curValue = lerp(startValue,keys[keyNum].targetValue,elapsedTime/(keys[keyNum].time*1000))
 			}else if(keys[keyNum].interpolation == 'ease'){
-				//Put in code for easing here
-				//var curTime=(elapsedTime/(keys[keyNum].time*1000))
-				//make this less confusing
 				curTime = (3*curTime*curTime)-(2*curTime*curTime*curTime)
 				curValue=startValue+(keys[keyNum].targetValue-startValue)*curTime
 			}
-				console.log('setting value to: '+curValue)
+			//console.log('setting value to: '+curValue)
 			Timeline.set(variable.name,curValue)
 		}else if(keyNum<keys.length-1){
 			startValue = window[variable.name]
@@ -135,7 +138,6 @@ Timeline.prototype.play = function(){
 			setTimeout(animate,1000/getFrameRate())
 		}	
 		isPlaying = false
-	//	console.log(someVar)
 	}
 		
 	animate()
